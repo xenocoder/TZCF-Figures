ef2424a98ae22b9905568b4386b3088f6ba72372
Evan Howell <admin@xenocoder.com>
2014-04-16 11:16:40 -1000

    redo figures

--- Figure 9 - Integrated HPLC/bin2d.m
+++ Figure 9 - Integrated HPLC/bin2d.m
@@ -13,22 +13,26 @@ if isstruct(in) == 0
     return
 endif
 
-XI = 1:175; %Make spline based on pressure 0-175
+XI = 1:150; %Make spline based on pressure 0-175
 
 out = zeros(5,4);
 count = 1;
 
-%for i = 26:2:34 %All but 1998
-for i = 30 %1998
+for i = 26:2:34 %All but 1998
+%for i = 26:2:30 %1998
     ind = find(in.latitude(1,:)>=i&in.latitude(1,:)<i+2);
     x = in.pressure(:,ind(end));
     prok = nanmean(in.chlaprok(:,ind),2);
+    %1998 ONLY!!!
+    %prok(7) = mean([prok(6) prok(8)]); %interp missing value
+    %prok(9) = prok(8); % repeast missing value
+    %1998 ONLY END
     euk = nanmean(in.chlaeuk(:,ind),2);
-    tc = nanmean(in.chlatotal(:,ind),2);
-    prokspline = spline(x,prok,XI); %do spline of Y at 1:175 (as 2009 only goes to 175)
-    eukspline = spline(x,euk,XI); %do spline of Y at 1:175 (as 2009 only goes to 175)
-    tcspline = spline(x,tc,XI); %do spline of Y at 1:175 (as 2009 only goes to 175)
-    out(count,:) = [i sum(prokspline)./1000 sum(eukspline)./1000 sum(tcspline)./1000];
+    tc = prok+euk;
+    prokspline = spline(x,prok,XI); %do spline of Y at 1:150 (as 1998 only goes to 175)
+    eukspline = spline(x,euk,XI); %do spline of Y at 1:150 (as 1998 only goes to 175)
+    tcspline = prokspline+eukspline;
+    out(count,:) = [i sum(prokspline)./1000 sum(eukspline)./1000 sum(tcspline)./1000]
     count = count + 1;
 endfor
 
