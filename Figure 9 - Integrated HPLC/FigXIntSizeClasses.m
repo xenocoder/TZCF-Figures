f3407e0b4fd6d221a22ece59f741f723762287eb
Evan Howell <evan.howell@noaa.gov>
2014-11-10 16:31:57 -1000

    Figure 9 bin code updated

--- Figure 9 - Integrated HPLC/FigXIntSizeClasses.m
+++ Figure 9 - Integrated HPLC/FigXIntSizeClasses.m
@@ -13,6 +13,12 @@ XI = 1:150; %Make spline based on pressure 0-175
 picopro = zeros(3,3);
 picoeuk = zeros(3,3);
 nanoeuk = zeros(3,3);
+
+prochl = zeros(3,3);
+prymchl = zeros(3,3);
+pelchl = zeros(3,3);
+diatchl = zeros(3,3);
+dinochl = zeros(3,3);
 otherchl = zeros(3,3);
 totalchl = zeros(3,3);
 
@@ -21,57 +27,51 @@ ssta = stationlistHPLC.SSTFZ(1,1);
 esta = stationlistHPLC.SSTFZ(1,2);
 x = SE0802.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE0802.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE0802.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE0802.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE0802.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE0802.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE0802.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE0802.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(1,1), ~] = calcspline(SE0802.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(1,1), ~] = calcspline(SE0802.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(1,1), ~] = calcspline(SE0802.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(1,1), ~] = calcspline(SE0802.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(1,1), ~] = calcspline(SE0802.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(1,1), ~] = calcspline(SE0802.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(1,1), ~] = calcspline(SE0802.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
-picopro(1,1) = proint + otherint;
-picoeuk(1,1) = prymint + pelint;
-nanoeuk(1,1) = diatint + dinoint;
-otherchl(1,1) = otherint;
-totalchl(1,1) = totalint;
+picopro(1,1) = prochl(1,1) + otherchl(1,1);
+picoeuk(1,1) = prymchl(1,1) + pelchl(1,1);
+nanoeuk(1,1) = diatchl(1,1) + dinochl(1,1);
 
 %2008 STFZ
 ssta = stationlistHPLC.STFZ(1,1);
 esta = stationlistHPLC.STFZ(1,2);
 x = SE0802.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE0802.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE0802.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE0802.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE0802.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE0802.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE0802.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE0802.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(1,2), ~] = calcspline(SE0802.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(1,2), ~] = calcspline(SE0802.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(1,2), ~] = calcspline(SE0802.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(1,2), ~] = calcspline(SE0802.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(1,2), ~] = calcspline(SE0802.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(1,2), ~] = calcspline(SE0802.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(1,2), ~] = calcspline(SE0802.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
-picopro(1,2) = proint + otherint;
-picoeuk(1,2) = prymint + pelint;
-nanoeuk(1,2) = diatint + dinoint;
-otherchl(1,2) = otherint;
-totalchl(1,2) = totalint;
+picopro(1,2) = prochl(1,2) + otherchl(1,2);
+picoeuk(1,2) = prymchl(1,2) + pelchl(1,2);
+nanoeuk(1,2) = diatchl(1,2) + dinochl(1,2);
 
 %2008 TZCF
 ssta = stationlistHPLC.TZCF(1,1);
 esta = stationlistHPLC.TZCF(1,2);
 x = SE0802.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE0802.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE0802.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE0802.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE0802.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE0802.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE0802.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE0802.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(1,3), ~] = calcspline(SE0802.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(1,3), ~] = calcspline(SE0802.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(1,3), ~] = calcspline(SE0802.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(1,3), ~] = calcspline(SE0802.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(1,3), ~] = calcspline(SE0802.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(1,3), ~] = calcspline(SE0802.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(1,3), ~] = calcspline(SE0802.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
-picopro(1,3) = proint + otherint;
-picoeuk(1,3) = prymint + pelint;
-nanoeuk(1,3) = diatint + dinoint;
-otherchl(1,3) = otherint;
-totalchl(1,3) = totalint;
+picopro(1,3) = prochl(1,3) + otherchl(1,3);
+picoeuk(1,3) = prymchl(1,3) + pelchl(1,3);
+nanoeuk(1,3) = diatchl(1,3) + dinochl(1,3);
 
 %Calculate 2009
 %2009 SSTFZ
@@ -79,116 +79,104 @@ ssta = stationlistHPLC.SSTFZ(2,1);
 esta = stationlistHPLC.SSTFZ(2,2);
 x = SE0902.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE0902.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE0902.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE0902.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE0902.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE0902.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE0902.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE0902.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(2,1), ~] = calcspline(SE0902.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(2,1), ~] = calcspline(SE0902.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(2,1), ~] = calcspline(SE0902.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(2,1), ~] = calcspline(SE0902.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(2,1), ~] = calcspline(SE0902.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(2,1), ~] = calcspline(SE0902.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(2,1), ~] = calcspline(SE0902.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
-picopro(2,1) = proint + otherint;
-picoeuk(2,1) = prymint + pelint;
-nanoeuk(2,1) = diatint + dinoint;
-otherchl(2,1) = otherint;
-totalchl(2,1) = totalint;
+picopro(2,1) = prochl(2,1) + otherchl(2,1);
+picoeuk(2,1) = prymchl(2,1) + pelchl(2,1);
+nanoeuk(2,1) = diatchl(2,1) + dinochl(2,1);
 
 %2009 STFZ
 ssta = stationlistHPLC.STFZ(2,1);
 esta = stationlistHPLC.STFZ(2,2);
 x = SE0902.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE0902.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE0902.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE0902.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE0902.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE0902.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE0902.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE0902.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(2,2), ~] = calcspline(SE0902.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(2,2), ~] = calcspline(SE0902.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(2,2), ~] = calcspline(SE0902.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(2,2), ~] = calcspline(SE0902.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(2,2), ~] = calcspline(SE0902.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(2,2), ~] = calcspline(SE0902.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(2,2), ~] = calcspline(SE0902.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
-picopro(2,2) = proint + otherint;
-picoeuk(2,2) = prymint + pelint;
-nanoeuk(2,2) = diatint + dinoint;
-otherchl(2,2) = otherint;
-totalchl(2,2) = totalint;
+picopro(2,2) = prochl(2,2) + otherchl(2,2);
+picoeuk(2,2) = prymchl(2,2) + pelchl(2,2);
+nanoeuk(2,2) = diatchl(2,2) + dinochl(2,2);
 
 %2009 TZCF
 ssta = stationlistHPLC.TZCF(2,1);
 esta = stationlistHPLC.TZCF(2,2);
 x = SE0902.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE0902.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE0902.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE0902.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE0902.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE0902.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE0902.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE0902.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(2,3), ~] = calcspline(SE0902.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(2,3), ~] = calcspline(SE0902.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(2,3), ~] = calcspline(SE0902.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(2,3), ~] = calcspline(SE0902.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(2,3), ~] = calcspline(SE0902.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(2,3), ~] = calcspline(SE0902.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(2,3), ~] = calcspline(SE0902.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
-picopro(2,3) = proint + otherint;
-picoeuk(2,3) = prymint + pelint;
-nanoeuk(2,3) = diatint + dinoint;
-otherchl(2,3) = otherint;
-totalchl(2,3) = totalint;
+picopro(2,3) = prochl(2,3) + otherchl(2,3);
+picoeuk(2,3) = prymchl(2,3) + pelchl(2,3);
+nanoeuk(2,3) = diatchl(2,3) + dinochl(2,3);
 
 %2011 Southern STFZ
 ssta = stationlistHPLC.SSTFZ(3,1);
 esta = stationlistHPLC.SSTFZ(3,2);
 x = SE1102.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE1102.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE1102.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE1102.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE1102.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE1102.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE1102.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE1102.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(3,1), ~] = calcspline(SE1102.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(3,1), ~] = calcspline(SE1102.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(3,1), ~] = calcspline(SE1102.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(3,1), ~] = calcspline(SE1102.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(3,1), ~] = calcspline(SE1102.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(3,1), ~] = calcspline(SE1102.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(3,1), ~] = calcspline(SE1102.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
 
-picopro(3,1) = proint + otherint;
-picoeuk(3,1) = prymint + pelint;
-nanoeuk(3,1) = diatint + dinoint;
-otherchl(3,1) = otherint;
-totalchl(3,1) = totalint;
+picopro(3,1) = prochl(3,1) + otherchl(3,1);
+picoeuk(3,1) = prymchl(3,1) + pelchl(3,1);
+nanoeuk(3,1) = diatchl(3,1) + dinochl(3,1);
 
 %2011 STFZ
 ssta = stationlistHPLC.STFZ(3,1);
 esta = stationlistHPLC.STFZ(3,2);
 x = SE1102.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE1102.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE1102.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE1102.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE1102.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE1102.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE1102.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE1102.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(3,2), ~] = calcspline(SE1102.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(3,2), ~] = calcspline(SE1102.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(3,2), ~] = calcspline(SE1102.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(3,2), ~] = calcspline(SE1102.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(3,2), ~] = calcspline(SE1102.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(3,2), ~] = calcspline(SE1102.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(3,2), ~] = calcspline(SE1102.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
 
-picopro(3,2) = proint + otherint;
-picoeuk(3,2) = prymint + pelint;
-nanoeuk(3,2) = diatint + dinoint;
-otherchl(3,2) = otherint;
-totalchl(3,2) = totalint;
+picopro(3,2) = prochl(3,2) + otherchl(3,2);
+picoeuk(3,2) = prymchl(3,2) + pelchl(3,2);
+nanoeuk(3,2) = diatchl(3,2) + dinochl(3,2);
 
 %2011 TZCF
 ssta = stationlistHPLC.TZCF(3,1);
 esta = stationlistHPLC.TZCF(3,2);
 x = SE1102.HPLC.pigrats.pressure(:,ssta);
 
-[proint, ~] = calcspline(SE1102.HPLC.pigrats.chlapro, x, XI, ssta, esta);
-[prymint, ~] = calcspline(SE1102.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
-[pelint, ~] = calcspline(SE1102.HPLC.pigrats.chlapel, x, XI, ssta, esta);
-[diatint, ~] = calcspline(SE1102.HPLC.pigrats.chladiat, x, XI, ssta, esta);
-[dinoint, ~] = calcspline(SE1102.HPLC.pigrats.chladino, x, XI, ssta, esta);
-[otherint, ~] = calcspline(SE1102.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
-[totalint, ~] = calcspline(SE1102.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
+[prochl(3,3), ~] = calcspline(SE1102.HPLC.pigrats.chlapro, x, XI, ssta, esta);
+[prymchl(3,3), ~] = calcspline(SE1102.HPLC.pigrats.chlaprym, x, XI, ssta, esta);
+[pelchl(3,3), ~] = calcspline(SE1102.HPLC.pigrats.chlapel, x, XI, ssta, esta);
+[diatchl(3,3), ~] = calcspline(SE1102.HPLC.pigrats.chladiat, x, XI, ssta, esta);
+[dinochl(3,3), ~] = calcspline(SE1102.HPLC.pigrats.chladino, x, XI, ssta, esta);
+[otherchl(3,3), ~] = calcspline(SE1102.HPLC.pigrats.chlaothers, x, XI, ssta, esta);
+[totalchl(3,3), ~] = calcspline(SE1102.HPLC.pigrats.chlatotal, x, XI, ssta, esta);
 
 
-picopro(3,3) = proint + otherint;
-picoeuk(3,3) = prymint + pelint;
-nanoeuk(3,3) = diatint + dinoint;
-otherchl(3,3) = otherint;
-totalchl(3,3) = totalint;
+picopro(3,3) = prochl(3,3) + otherchl(3,3);
+picoeuk(3,3) = prymchl(3,3) + pelchl(3,3);
+nanoeuk(3,3) = diatchl(3,3) + dinochl(3,3);
 
-clear proint prymint pelint diatint dinoint otherint totalint ssta esta x XI 
+clear ssta esta x XI 
