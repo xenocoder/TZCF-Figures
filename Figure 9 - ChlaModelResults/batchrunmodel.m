bcafeec6e1f8e22c8e63137ea5144b2c70a34a0e
Evan Howell <admin@xenocoder.com>
2014-11-20 18:23:58 -1000

    Figure 11 Median Cell Size code

---
+++ Figure 9 - ChlaModelResults/batchrunmodel.m
@@ -0,0 +1,3 @@
+SE0802.HPLC.hirata.model = calchiratamodel(SE0802.HPLC.hirata);
+SE0902.HPLC.hirata.model = calchiratamodel(SE0902.HPLC.hirata);
+SE1102.HPLC.hirata.model = calchiratamodel(SE1102.HPLC.hirata);
