d8a15c50b3e8fbadd70beab02e39b7450beecf00
Evan Howell <admin@xenocoder.com>
2014-11-24 22:58:24 -1000

    Figure 10 and stats

--- Figure 10 - BarChart SizeClass Statistics/ReadMeanSEData.r
+++ Figure 10 - BarChart SizeClass Statistics/ReadMeanSEData.r
@@ -9,65 +9,12 @@ rm(list=ls())
 
 # Read in the file that Matlab wrote out (CSV). chlpigdata.csv is actual values
 dat = read.csv('meanphyto-werror.csv',header=T)
+pig = read.csv('pigmeanplussem.csv',header=T)
 
-#Aggregate mean and se across years
-dat.zone = aggregate(dat[,4:5],by=list(dat$PSC,dat$zone),FUN=mean)
+ggplot() + geom_bar(data=pig, aes(y = mnval, x = Zone), stat="identity", fill = I("grey50")) + theme_bw() + facet_grid( ~ PSC) + geom_errorbar(data=pig, aes(x=Zone, ymin=mnval-sem,ymax=mnval+sem))
 
-#Change 1,2 to '1-STFZ','2-TZCF' since Matlab only writes numbers
-pig$zone[pig$zone==1]='1-STFZ'
-pig$zone[pig$zone==2]='2-TZCF'
-
-repig.all = melt(pig[,c(1:2,4:10)],id=c('year','zone')) #Reshape to columns only taking chloropigments (e.g. pro, prym, diat, etc)
-repig.size = melt(pig[,c(1:2,11:13)],id=c('year','zone')) # Reshape but only pico, nano, micro groups
-
-# Barplot of all groups in all areas grouped by year and zone
-ggplot() + geom_bar(data=repig.all, aes(y = mn, x = zone, fill = PSC), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
-
-# Barplot of pico, nano, micro in all areas grouped by year and zone
-ggplot() + geom_bar(data=repig.size, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
-
-# Do same thing but as percentage
-pigperc = cbind(pig[,1:2],pig[4:10]/rowSums(pig[4:10]))
-
-repigperc.all = melt(pigperc,id=c('year','zone')) #Reshape to columns only taking chloropigments (e.g. pro, prym, diat, etc)
-
-# Barplot of all groups in all areas grouped by year and zone
-ggplot() + geom_bar(data=repigperc.all, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
-
-# Now do the size classes
-pigperc = cbind(pig[,1:2],pig[11:13]/rowSums(pig[11:13]))
-
-repigperc.size = melt(pigperc,id=c('year','zone')) #Reshape to columns only taking chloropigments (e.g. pro, prym, diat, etc)
-
-# Barplot of pico, nano, micro in all areas grouped by year and zone
-ggplot() + geom_bar(data=repigperc.size, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
-
-###############FOR SURFACE DATA###################
-pigsurf = read.csv('chlpigdatahirata-surface.csv', header=T)
-pigsurf$zone[pigsurf$zone==1]='1-STFZ'
-pigsurf$zone[pigsurf$zone==2]='2-TZCF'
-
-repigsurf.all = melt(pigsurf[,c(1:2,4:10)],id=c('year','zone')) #Reshape to columns only taking chloropigments (e.g. pro, prym, diat, etc)
-repigsurf.size = melt(pigsurf[,c(1:2,11:13)],id=c('year','zone')) # Reshape but only pico, nano, micro groups
-
-# Barplot of pico, nano, micro in all areas grouped by year and zone
-ggplot() + geom_bar(data=repigsurf.size, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
-
-# Barplot of all groups in all areas grouped by year and zone
-ggplot() + geom_bar(data=repigsurf.all, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
-
-# Do same thing but as percentage
-pigsurfperc = cbind(pigsurf[,1:2],pigsurf[4:10]/rowSums(pigsurf[4:10]))
-
-repigsurfperc.all = melt(pigsurfperc,id=c('year','zone')) #Reshape to columns only taking chloropigments (e.g. pro, prym, diat, etc)
-
-# Barplot of pico, nano, micro in all areas grouped by year and zone
-ggplot() + geom_bar(data=repigsurfperc.all, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
-
-# Now do the size classes
-pigsurfperc = cbind(pigsurf[,1:2],pigsurf[11:13]/rowSums(pigsurf[11:13]))
-
-repigsurfperc.size = melt(pigsurfperc,id=c('year','zone')) #Reshape to columns only taking chloropigments (e.g. pro, prym, diat, etc)
-
-# Barplot of pico, nano, micro in all areas grouped by year and zone
-ggplot() + geom_bar(data=repigsurfperc.size, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
+pico = dat[dat$PSC=="pico",]
+nano = dat[dat$PSC=="nano",]
+micro = dat[dat$PSC=="micro",]
+interaction.plot(pico$year,pico$zone,pico$mn)
+interaction.plot(pico$zone,pico$year,pico$mn)
