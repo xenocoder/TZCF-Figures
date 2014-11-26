b3353b48539d77cc45f323afe935fd78dcae6040
Evan Howell <admin@xenocoder.com>
2014-11-26 12:32:34 -1000

    Switched figures 4 and 5, and 6 and 7

--- Figure 03 - TempSalDens Profiles/NOTUSEDPlotTempNO3Chla.m
+++ Figure 03 - TempSalDens Profiles/NOTUSEDPlotTempNO3Chla.m
@@ -1,9 +1,69 @@
 %MATLAB script to plot 2008, 2009, and 2011 temperature
 
+%%%%%%%%%%%%%  TEMPERATURE %%%%%%%%%%%%%%%%%%%%
 %2008
-%Nitrate STFZ
+%Subtropical Front (18-19°C)
 clf
 subplot(2,3,1)
+ssta = stationlist.STFZ(1,1);
+esta = stationlist.STFZ(1,2);
+x = mean(SE0802.temperature(1:196,ssta:esta),2);
+y = SE0802.pressure(1:196,ssta);
+plot(x,y,'b-','linewidth',5);
+set(gca, 'xlim',[11 19])
+axis ij
+hold on
+
+ssta = stationlist.STFZ(2,1);
+esta = stationlist.STFZ(2,2);
+x = mean(SE0902.temperature(1:188,ssta:esta),2);
+y = SE0902.pressure(1:188,ssta);
+plot(x,y,'r-','linewidth',5)
+
+ssta = stationlist.STFZ(3,1);
+esta = stationlist.STFZ(3,2);
+x = mean(SE1102.temperature(1:196,ssta:esta),2);
+y = SE1102.pressure(1:196,ssta);
+plot(x,y,'k-','linewidth',5)
+
+legend('2008','2009','2011','Location','southeast')
+title('Mean Temperature at STFZ')
+xlabel('Temperature (°C)')
+ylabel('Pressure (db)')
+
+%TZCF%
+subplot(2,3,4)
+ssta = stationlist.TZCF(1,1);
+esta = stationlist.TZCF(1,2);
+x = mean(SE0802.temperature(1:196,ssta:esta),2);
+y = SE0802.pressure(1:196,ssta);
+plot(x,y,'b-','linewidth',5)
+set(gca, 'xlim',[11 19])
+axis ij
+hold on
+
+ssta = stationlist.TZCF(2,1);
+esta = stationlist.TZCF(2,2);
+x = mean(SE0902.temperature(1:188,ssta:esta),2);
+y = SE0902.pressure(1:188,ssta);
+plot(x,y,'r-','linewidth',5)
+
+ssta = stationlist.TZCF(3,1);
+esta = stationlist.TZCF(3,2);
+x = mean(SE1102.temperature(1:196,ssta:esta),2);
+y = SE1102.pressure(1:196,ssta);
+plot(x,y,'k-','linewidth',5)
+
+legend('2008','2009','2011','Location','southeast')
+title('Mean Temperature at TZCF')
+xlabel('Temperature (°C)')
+ylabel('Pressure (db)')
+
+%MATLAB script to plot 2008, 2009, and 2011 temperature
+
+%%%%%%%%%%%%%  NITRATE %%%%%%%%%%%%%%%%%%%%
+%Nitrate STFZ
+subplot(2,3,2)
 ssta = stationlistNUTS.STFZ(1,1);
 esta = stationlistNUTS.STFZ(1,2);
 x = nanmean(SE0802.NUTS.nitrate(:,ssta:esta),2);
@@ -33,7 +93,7 @@ xlabel('Nitrate (umol kg-1)')
 ylabel('Pressure (db)')
 
 %TZCF%
-subplot(2,3,4)
+subplot(2,3,5)
 ssta = stationlistNUTS.TZCF(1,1);
 esta = stationlistNUTS.TZCF(1,2);
 x = nanmean(SE0802.NUTS.nitrate(:,ssta:esta),2);
@@ -62,71 +122,15 @@ title('Mean Nitrate at TZCF')
 xlabel('Nitrate (umol kg-1)')
 ylabel('Pressure (db)')
 
-% %Oxygen
-% subplot(2,3,2)
-% ssta = stationlist.STFZ(1,1);
-% esta = stationlist.STFZ(1,2);
-% x = nanmean(SE0802.oxygen(:,ssta:esta),2);
-% y = SE0802.pressure(:,ssta);
-% plot(x,y,'b-','linewidth',5);
-% set(gca, 'xlim',[0 12])
-% axis ij
-% hold on
-% 
-% ssta = stationlist.STFZ(2,1);
-% esta = stationlist.STFZ(2,2);
-% x = nanmean(SE0902.oxygen(:,ssta:esta),2);
-% y = SE0902.pressure(:,ssta);
-% plot(x,y,'r-','linewidth',5)
-% 
-% ssta = stationlist.STFZ(3,1);
-% esta = stationlist.STFZ(3,2);
-% x = nanmean(SE1102.oxygen(:,ssta:esta),2);
-% y = SE1102.pressure(:,ssta);
-% plot(x,y,'k-','linewidth',5)
-% 
-% legend('2008','2009','2011','Location','northeast')
-% title('Mean Oxygen at STFZ')
-% xlabel('oxygen (mmol kg-1)')
-% ylabel('Pressure (db)')
-% 
-% %TZCF%
-% subplot(2,3,5)
-% ssta = stationlist.TZCF(1,1);
-% esta = stationlist.TZCF(1,2);
-% x = nanmean(SE0802.oxygen(:,ssta:esta),2);
-% y = SE0802.pressure(:,ssta);
-% plot(x,y,'b-','linewidth',5)
-% set(gca, 'xlim',[0 12])
-% axis ij
-% hold on
-% 
-% ssta = stationlist.TZCF(2,1);
-% esta = stationlist.TZCF(2,2);
-% x = nanmean(SE0902.oxygen(:,ssta:esta),2);
-% y = SE0902.pressure(:,ssta);
-% plot(x,y,'r-','linewidth',5)
-% 
-% ssta = stationlist.TZCF(3,1);
-% esta = stationlist.TZCF(3,2);
-% x = nanmean(SE1102.oxygen(:,ssta:esta),2);
-% y = SE1102.pressure(:,ssta);
-% plot(x,y,'k-','linewidth',5)
-% 
-% legend('2008','2009','2011','Location','northeast')
-% title('Mean Oxygen at TZCF')
-% xlabel('Oxygen (umol kg-1)')
-% ylabel('Pressure (db)')
-% 
-%Fluorescence
+%%%%%%%%%%%%%  CHLOROPHYLL %%%%%%%%%%%%%%%%%%%%
 
-subplot(2,3,2)
+subplot(2,3,3)
 ssta = stationlist.STFZ(1,1);
 esta = stationlist.STFZ(1,2);
 x = mean(SE0802.fluorcorrected(1:196,ssta:esta),2);
 y = SE0802.pressure(1:196,ssta);
-plot(x,y,'b-','linewidth',5)
-set(gca, 'xlim', [0 0.8])
+plot(x./max(x),y,'b-','linewidth',5)
+set(gca, 'xlim', [0 1.2])
 axis ij
 hold on
 
@@ -135,27 +139,27 @@ ssta = stationlist.STFZ(2,1);
 esta = stationlist.STFZ(2,2);
 x = mean(SE0902.fluorcorrected(1:188,ssta:esta),2);
 y = SE0902.pressure(1:188,ssta);
-plot(x,y,'r-','linewidth',5)
+plot(x./max(x),y,'r-','linewidth',5)
 
 %2011
 ssta = stationlist.STFZ(3,1);
 esta = stationlist.STFZ(3,2);
 x = mean(SE1102.fluorcorrected(1:196,ssta:esta),2);
 y = SE1102.pressure(1:196,ssta);
-plot(x,y,'k-','linewidth',5)
+plot(x./max(x),y,'k-','linewidth',5)
 
 legend('2008','2009','2011','Location','southeast')
 title('Mean Fluorescence at STFZ')
-xlabel('fluorcorrected (°C)')
+xlabel('Fluorescence (mg m^-3)')
 ylabel('Pressure (db)')
 
 %%%TZCF%%%
-subplot(2,3,5)
+subplot(2,3,6)
 ssta = stationlist.TZCF(1,1);
 esta = stationlist.TZCF(1,2);
 x = mean(SE0802.fluorcorrected(1:196,ssta:esta),2);
 y = SE0802.pressure(1:196,ssta);
-plot(x,y,'b-','linewidth',5)
+plot(x./max(x),y,'b-','linewidth',5)
 axis ij
 hold on
 
@@ -164,17 +168,17 @@ ssta = stationlist.TZCF(2,1);
 esta = stationlist.TZCF(2,2);
 x = mean(SE0902.fluorcorrected(1:188,ssta:esta),2);
 y = SE0902.pressure(1:188,ssta);
-plot(x,y,'r-','linewidth',5)
+plot(x./max(x),y,'r-','linewidth',5)
 
 %2011
 ssta = stationlist.TZCF(3,1);
 esta = stationlist.TZCF(3,2);
 x = mean(SE1102.fluorcorrected(1:196,ssta:esta),2);
 y = SE1102.pressure(1:196,ssta);
-plot(x,y,'k-','linewidth',5)
-set(gca, 'xlim', [0 0.8])
+plot(x./max(x),y,'k-','linewidth',5)
+set(gca, 'xlim', [0 1.2])
 
 legend('2008','2009','2011','Location','southeast')
 title('Mean Fluorescence at TZCF')
-xlabel('fluorcorrected (°C)')
+xlabel('Fluorescence (mg m^-3)')
 ylabel('Pressure (db)')
---
+++ Figure 05 - Model Pigments Splines All/PlotSplinesAllYearsNoTotalChla.m
@@ -0,0 +1,281 @@
+%Plot splines for Hirata equation values
+picopl = zeros(3,2);
+nanopl = zeros(3,2);
+micropl = zeros(3,2);
+
+proc = zeros(3,2);
+prok = zeros(3,2);
+galgae = zeros(3,2);
+crypto = zeros(3,2);
+pelago = zeros(3,2);
+prym = zeros(3,2);
+dino = zeros(3,2);
+diat = zeros(3,2);
+SDP = zeros(3,2);
+totalchl = zeros(3,2);
+
+%2008 STFZ
+XI = 1:200; %Make spline based on pressure 0-175
+ssta = stationlistHPLC.STFZ(1,1);
+esta = stationlistHPLC.STFZ(1,2);
+x = SE0802.HPLC.pigrats.pressure(:,ssta);
+
+%deal with missing values at 100,125m.
+prokspline = interphplc08(SE0802.HPLC.hirata.prok(:,ssta:esta),x,1:200);
+galgaespline = interphplc08(SE0802.HPLC.hirata.galgae(:,ssta:esta),x,1:200);
+cryptospline = interphplc08(SE0802.HPLC.hirata.crypto(:,ssta:esta),x,1:200);
+pelagospline = interphplc08(SE0802.HPLC.hirata.pelago(:,ssta:esta),x,1:200);
+prymspline = interphplc08(SE0802.HPLC.hirata.prym(:,ssta:esta),x,1:200);
+dinospline = interphplc08(SE0802.HPLC.hirata.dino(:,ssta:esta),x,1:200);
+diatspline = interphplc08(SE0802.HPLC.hirata.diatoms(:,ssta:esta),x,1:200);
+SDPspline = interphplc08(SE0802.HPLC.hirata.SDP(:,ssta:esta),x,1:200);
+totalspline = interphplc08(SE0802.HPLC.hirata.C(:,ssta:esta),x,1:200);
+
+%set all values lower than
+%[~, prokspline] = calcspline(SE0802.HPLC.hirata.prok, x, XI, ssta, esta);
+%[~, galgaespline] = calcspline(SE0802.HPLC.hirata.galgae, x, XI, ssta, esta);
+%[~, cryptospline] = calcspline(SE0802.HPLC.hirata.crypto, x, XI, ssta, esta);
+% [~, pelagospline] = calcspline(SE0802.HPLC.hirata.pelago, x, XI, ssta, esta);
+% [~, prymspline] = calcspline(SE0802.HPLC.hirata.prym, x, XI, ssta, esta);
+% [~, dinospline] = calcspline(SE0802.HPLC.hirata.dino, x, XI, ssta, esta);
+% [~, diatspline] = calcspline(SE0802.HPLC.hirata.diatoms, x, XI, ssta, esta);
+% [~, SDPspline] = calcspline(SE0802.HPLC.hirata.SDP, x, XI, ssta, esta);
+% [~, totalspline] = calcspline(SE0802.HPLC.hirata.C, x, XI, ssta, esta);
+
+% prokspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% galgaespline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% cryptospline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% pelagospline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% prymspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% dinospline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% diatspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% SDPspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% totalspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+
+clf
+subplot(2,3,1)
+plot(prokspline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'b-','LineWidth',3)
+axis ij
+set(gca, 'XLim', [0 0.3], 'YLim', [0 180])
+hold on
+plot(galgaespline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'g-','LineWidth',3)
+%plot(cryptospline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'c--','LineWidth',3)
+plot(pelagospline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'r-','LineWidth',3)
+plot(prymspline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'m-','LineWidth',3)
+plot(dinospline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'c-','LineWidth',3)
+plot(diatspline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'r--','LineWidth',3)
+%plot(totalspline(1:175)./1000, XI,'k--','LineWidth',3)
+
+title('Phytoplankton Composition at STFZ in 2008')
+xlabel('Chl-a (mg m-3)')
+ylabel('Pressure (db)')
+%legend('Location','southeast', 'Prokaryotes','Green Algae','Cryptophytes','Pelagophytes', 'Prymnesio','Dinoflag','Diatoms')
+
+%2008 TZCF
+ssta = stationlistHPLC.TZCF(1,1);
+esta = stationlistHPLC.TZCF(1,2);
+x = SE0802.HPLC.pigrats.pressure(:,ssta);
+
+%deal with missing values at 100,125m.
+prokspline = interphplc08(SE0802.HPLC.hirata.prok(:,ssta:esta),x,1:200);
+galgaespline = interphplc08(SE0802.HPLC.hirata.galgae(:,ssta:esta),x,1:200);
+cryptospline = interphplc08(SE0802.HPLC.hirata.crypto(:,ssta:esta),x,1:200);
+pelagospline = interphplc08(SE0802.HPLC.hirata.pelago(:,ssta:esta),x,1:200);
+prymspline = interphplc08(SE0802.HPLC.hirata.prym(:,ssta:esta),x,1:200);
+dinospline = interphplc08(SE0802.HPLC.hirata.dino(:,ssta:esta),x,1:200);
+diatspline = interphplc08(SE0802.HPLC.hirata.diatoms(:,ssta:esta),x,1:200);
+SDPspline = interphplc08(SE0802.HPLC.hirata.SDP(:,ssta:esta),x,1:200);
+totalspline = interphplc08(SE0802.HPLC.hirata.C(:,ssta:esta),x,1:200);
+
+% [~, prokspline] = calcspline(SE0802.HPLC.hirata.prok, x, XI, ssta, esta);
+% [~, galgaespline] = calcspline(SE0802.HPLC.hirata.galgae, x, XI, ssta, esta);
+% [~, cryptospline] = calcspline(SE0802.HPLC.hirata.crypto, x, XI, ssta, esta);
+% [~, pelagospline] = calcspline(SE0802.HPLC.hirata.pelago, x, XI, ssta, esta);
+% [~, prymspline] = calcspline(SE0802.HPLC.hirata.prym, x, XI, ssta, esta);
+% [~, dinospline] = calcspline(SE0802.HPLC.hirata.dino, x, XI, ssta, esta);
+% [~, diatspline] = calcspline(SE0802.HPLC.hirata.diatoms, x, XI, ssta, esta);
+% [~, SDPspline] = calcspline(SE0802.HPLC.hirata.SDP, x, XI, ssta, esta);
+% [~, totalspline] = calcspline(SE0802.HPLC.hirata.C, x, XI, ssta, esta);
+% 
+% prokspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% galgaespline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% cryptospline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% pelagospline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% prymspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% dinospline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% diatspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% SDPspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% totalspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+
+subplot(2,3,4)
+plot(prokspline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'b-','LineWidth',3)
+axis ij
+set(gca, 'XLim', [0 0.3], 'YLim', [0 180])
+hold on
+plot(galgaespline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'g-','LineWidth',3)
+%plot(cryptospline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'c--','LineWidth',3)
+plot(pelagospline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'r-','LineWidth',3)
+plot(prymspline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'m-','LineWidth',3)
+plot(dinospline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'c-','LineWidth',3)
+plot(diatspline(1:175)./SDPspline(1:175).*totalspline(1:175)./1000, XI(1:175),'r--','LineWidth',3)
+%plot(totalspline(1:175)./1000, XI,'k--','LineWidth',3)
+
+title('Phytoplankton Composition at TZCF in 2008')
+xlabel('Chl-a (mg m-3)')
+ylabel('Pressure (db)')
+%legend('Location','southeast', 'Prokaryotes','Green Algae','Cryptophytes','Pelagophytes', 'Prymnesio','Dinoflag','Diatoms')
+
+%2009 STFZ
+XI = 1:175; %Make spline based on pressure 0-175
+ssta = stationlistHPLC.STFZ(2,1);
+esta = stationlistHPLC.STFZ(2,2);
+x = SE0902.HPLC.pigrats.pressure(:,ssta);
+
+[~, prokspline] = calcinterp(SE0902.HPLC.hirata.prok, x, XI, ssta, esta);
+[~, galgaespline] = calcinterp(SE0902.HPLC.hirata.galgae, x, XI, ssta, esta);
+[~, cryptospline] = calcinterp(SE0902.HPLC.hirata.crypto, x, XI, ssta, esta);
+[~, pelagospline] = calcinterp(SE0902.HPLC.hirata.pelago, x, XI, ssta, esta);
+[~, prymspline] = calcinterp(SE0902.HPLC.hirata.prym, x, XI, ssta, esta);
+[~, dinospline] = calcinterp(SE0902.HPLC.hirata.dino, x, XI, ssta, esta);
+[~, diatspline] = calcinterp(SE0902.HPLC.hirata.diatoms, x, XI, ssta, esta);
+[~, SDPspline] = calcinterp(SE0902.HPLC.hirata.SDP, x, XI, ssta, esta);
+[~, totalspline] = calcinterp(SE0902.HPLC.hirata.C, x, XI, ssta, esta);
+
+subplot(2,3,2)
+plot(prokspline./SDPspline.*totalspline./1000, XI,'b-','LineWidth',3)
+axis ij
+set(gca, 'XLim', [0 0.3], 'YLim', [0 180])
+hold on
+plot(galgaespline./SDPspline.*totalspline./1000, XI,'g-','LineWidth',3)
+%plot(cryptospline./SDPspline.*totalspline./1000, XI,'c--','LineWidth',3)
+plot(pelagospline./SDPspline.*totalspline./1000, XI,'r-','LineWidth',3)
+plot(prymspline./SDPspline.*totalspline./1000, XI,'m-','LineWidth',3)
+plot(dinospline./SDPspline.*totalspline./1000, XI,'c-','LineWidth',3)
+plot(diatspline./SDPspline.*totalspline./1000, XI,'r--','LineWidth',3)
+%plot(totalspline./1000, XI,'k--','LineWidth',3)
+
+title('Phytoplankton Composition at STFZ in 2009')
+xlabel('Chl-a (mg m-3)')
+ylabel('Pressure (db)')
+%legend('Location','southeast', 'Prokaryotes','Green Algae','Cryptophytes','Pelagophytes', 'Prymnesio','Dinoflag','Diatoms')
+
+%2009 TZCF
+ssta = stationlistHPLC.TZCF(2,1);
+esta = stationlistHPLC.TZCF(2,2);
+x = SE0902.HPLC.pigrats.pressure(:,ssta);
+
+[~, prokspline] = calcinterp(SE0902.HPLC.hirata.prok, x, XI, ssta, esta);
+[~, galgaespline] = calcinterp(SE0902.HPLC.hirata.galgae, x, XI, ssta, esta);
+[~, cryptospline] = calcinterp(SE0902.HPLC.hirata.crypto, x, XI, ssta, esta);
+[~, pelagospline] = calcinterp(SE0902.HPLC.hirata.pelago, x, XI, ssta, esta);
+[~, prymspline] = calcinterp(SE0902.HPLC.hirata.prym, x, XI, ssta, esta);
+[~, dinospline] = calcinterp(SE0902.HPLC.hirata.dino, x, XI, ssta, esta);
+[~, diatspline] = calcinterp(SE0902.HPLC.hirata.diatoms, x, XI, ssta, esta);
+[~, SDPspline] = calcinterp(SE0902.HPLC.hirata.SDP, x, XI, ssta, esta);
+[~, totalspline] = calcinterp(SE0902.HPLC.hirata.C, x, XI, ssta, esta);
+
+subplot(2,3,5)
+plot(prokspline./SDPspline.*totalspline./1000, XI,'b-','LineWidth',3)
+axis ij
+set(gca, 'XLim', [0 0.3], 'YLim', [0 180])
+hold on
+plot(galgaespline./SDPspline.*totalspline./1000, XI,'g-','LineWidth',3)
+%plot(cryptospline./SDPspline.*totalspline./1000, XI,'c--','LineWidth',3)
+plot(pelagospline./SDPspline.*totalspline./1000, XI,'r-','LineWidth',3)
+plot(prymspline./SDPspline.*totalspline./1000, XI,'m-','LineWidth',3)
+plot(dinospline./SDPspline.*totalspline./1000, XI,'c-','LineWidth',3)
+plot(diatspline./SDPspline.*totalspline./1000, XI,'r--','LineWidth',3)
+%plot(totalspline./1000, XI,'k--','LineWidth',3)
+
+title('Phytoplankton Composition at TZCF in 2009')
+xlabel('Chl-a (mg m-3)')
+ylabel('Pressure (db)')
+%legend('Location','southeast', 'Prokaryotes','Green Algae','Cryptophytes','Pelagophytes', 'Prymnesio','Dinoflag','Diatoms')
+
+%2011 STFZ
+XI = 1:200; %Make spline based on pressure 0-175
+ssta = stationlistHPLC.STFZ(3,1);
+esta = stationlistHPLC.STFZ(3,2);
+x = SE1102.HPLC.pigrats.pressure(:,ssta);
+
+[~, prokspline] = calcinterp(SE1102.HPLC.hirata.prok, x, XI, ssta, esta);
+[~, galgaespline] = calcinterp(SE1102.HPLC.hirata.galgae, x, XI, ssta, esta);
+%[~, cryptospline] = calcspline(SE1102.HPLC.hirata.crypto, x, XI, ssta, esta);
+[~, pelagospline] = calcinterp(SE1102.HPLC.hirata.pelago, x, XI, ssta, esta);
+[~, prymspline] = calcinterp(SE1102.HPLC.hirata.prym, x, XI, ssta, esta);
+[~, dinospline] = calcinterp(SE1102.HPLC.hirata.dino, x, XI, ssta, esta);
+[~, diatspline] = calcinterp(SE1102.HPLC.hirata.diatoms, x, XI, ssta, esta);
+[~, SDPspline] = calcinterp(SE1102.HPLC.hirata.SDP, x, XI, ssta, esta);
+[~, totalspline] = calcinterp(SE1102.HPLC.hirata.C, x, XI, ssta, esta);
+
+% prokspline(1,160:200) = 0; %spline freaks out at lower levels but no real signal
+% galgaespline(1,160:200) = 0; %spline freaks out at lower levels but no real signal
+% cryptospline(1,160:200) = 0; %spline freaks out at lower levels but no real signal
+% pelagospline(1,160:200) = 0; %spline freaks out at lower levels but no real signal
+% %prymspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% dinospline(1,160:200) = 0; %spline freaks out at lower levels but no real signal
+% diatspline(1,160:200) = 0; %spline freaks out at lower levels but no real signal
+%SDPspline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+%totalspline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+
+subplot(2,3,3)
+plot(prokspline./SDPspline.*totalspline./1000, XI,'b-','LineWidth',3)
+axis ij
+set(gca, 'XLim', [0 0.3], 'YLim', [0 180])
+hold on
+plot(galgaespline./SDPspline.*totalspline./1000, XI,'g-','LineWidth',3)
+%plot(cryptospline./SDPspline.*totalspline./1000, XI,'c--','LineWidth',3)
+plot(pelagospline./SDPspline.*totalspline./1000, XI,'r-','LineWidth',3)
+plot(prymspline./SDPspline.*totalspline./1000, XI,'m-','LineWidth',3)
+plot(dinospline./SDPspline.*totalspline./1000, XI,'c-','LineWidth',3)
+plot(diatspline./SDPspline.*totalspline./1000, XI,'r--','LineWidth',3)
+%plot(totalspline./1000, XI,'k--','LineWidth',3)
+
+title('Phytoplankton Composition at STFZ in 2011')
+xlabel('Chl-a (mg m-3)')
+ylabel('Pressure (db)')
+%legend('Location','southeast', 'Prokaryotes','Green Algae','Cryptophytes','Pelagophytes', 'Prymnesio','Dinoflag','Diatoms')
+
+%2011 TZCF
+ssta = stationlistHPLC.TZCF(3,1);
+esta = stationlistHPLC.TZCF(3,2);
+x = SE1102.HPLC.pigrats.pressure(:,ssta);
+
+[~, prokspline] = calcinterp(SE1102.HPLC.hirata.prok, x, XI, ssta, esta);
+[~, galgaespline] = calcinterp(SE1102.HPLC.hirata.galgae, x, XI, ssta, esta);
+[~, cryptospline] = calcinterp(SE1102.HPLC.hirata.crypto, x, XI, ssta, esta);
+[~, pelagospline] = calcinterp(SE1102.HPLC.hirata.pelago, x, XI, ssta, esta);
+[~, prymspline] = calcinterp(SE1102.HPLC.hirata.prym, x, XI, ssta, esta);
+[~, dinospline] = calcinterp(SE1102.HPLC.hirata.dino, x, XI, ssta, esta);
+[~, diatspline] = calcinterp(SE1102.HPLC.hirata.diatoms, x, XI, ssta, esta);
+[~, SDPspline] = calcinterp(SE1102.HPLC.hirata.SDP, x, XI, ssta, esta);
+[~, totalspline] = calcinterp(SE1102.HPLC.hirata.C, x, XI, ssta, esta);
+
+% prokspline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+% galgaespline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+% cryptospline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+% pelagospline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+% prymspline(1,150:175) = 0; %spline freaks out at lower levels but no real signal
+% dinospline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+% diatspline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+% %SDPspline(1,160:175) = 0; %spline freaks out at lower levels but no real signal
+% totalspline(1,155:175) = 0; %spline freaks out at lower levels but no real signal
+
+subplot(2,3,6)
+plot(prokspline./SDPspline.*totalspline./1000, XI,'b-','LineWidth',3)
+axis ij
+set(gca, 'XLim', [0 0.3])
+hold on
+plot(galgaespline./SDPspline.*totalspline./1000, XI,'g-','LineWidth',3)
+%plot(cryptospline./SDPspline.*totalspline./1000, XI,'c--','LineWidth',3)
+plot(pelagospline./SDPspline.*totalspline./1000, XI,'r-','LineWidth',3)
+plot(prymspline./SDPspline.*totalspline./1000, XI,'m-','LineWidth',3)
+plot(dinospline./SDPspline.*totalspline./1000, XI,'c-','LineWidth',3)
+plot(diatspline./SDPspline.*totalspline./1000, XI,'r--','LineWidth',3)
+%plot(totalspline./1000, XI,'k--','LineWidth',3)
+
+title('Phytoplankton Composition at TZCF in 2011')
+xlabel('Chl-a (mg m-3)')
+ylabel('Pressure (db)')
+%legend('Location','southeast', 'Prokaryotes','Green Algae','Cryptophytes','Pelagophytes', 'Prymnesio','Dinoflag','Diatoms')
+legend('Location','southeast', 'Prokaryotes','Green Algae','Pelagophytes', 'Prymnesio','Dinoflag','Diatoms')
