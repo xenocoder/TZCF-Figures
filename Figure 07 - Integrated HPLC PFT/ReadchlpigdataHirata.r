871687e96a395182d4ce5beddca9cd4942bde7b6
Evan Howell <admin@xenocoder.com>
2014-11-28 12:11:18 -1000

    Work on possible wind figures and figure captions added

--- Figure 07 - Integrated HPLC PFT/ReadchlpigdataHirata.r
+++ Figure 07 - Integrated HPLC PFT/ReadchlpigdataHirata.r
@@ -5,8 +5,8 @@ library(reshape)
 library(ggplot2)
 
 # Read in the file that Matlab wrote out (CSV). chlpigdata.csv is actual values
-#pig = read.csv('chlpigdatahirata.csv',header=T)
-pig = read.csv('chlpigdatahirata-100m.csv',header=T)
+pig = read.csv('chlpigdatahirata.csv',header=T)
+#pig = read.csv('chlpigdatahirata-100m.csv',header=T)
 
 
 #Change 1,2 to '1-STFZ','2-TZCF' since Matlab only writes numbers
@@ -17,7 +17,7 @@ repig.all = melt(pig[,c(1:2,4:10)],id=c('year','zone')) #Reshape to columns only
 repig.size = melt(pig[,c(1:2,11:13)],id=c('year','zone')) # Reshape but only pico, nano, micro groups
 
 # Barplot of all groups in all areas grouped by year and zone
-ggplot() + geom_bar(data=repig.all, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
+ggplot() + geom_bar(data=repig.all, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year) + scale_fill_brewer(palette="Set1")
 
 # Barplot of pico, nano, micro in all areas grouped by year and zone
 ggplot() + geom_bar(data=repig.size, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
@@ -28,7 +28,7 @@ pigperc = cbind(pig[,1:2],pig[4:10]/rowSums(pig[4:10]))
 repigperc.all = melt(pigperc,id=c('year','zone')) #Reshape to columns only taking chloropigments (e.g. pro, prym, diat, etc)
 
 # Barplot of all groups in all areas grouped by year and zone
-ggplot() + geom_bar(data=repigperc.all, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year)
+ggplot() + geom_bar(data=repigperc.all, aes(y = value, x = zone, fill = variable), stat="identity", position='stack') + theme_bw() + facet_grid( ~ year) + scale_fill_brewer(palette="Set1")
 
 # Now do the size classes
 pigperc = cbind(pig[,1:2],pig[11:13]/rowSums(pig[11:13]))
---
+++ Figure X4 - Wind Fields/PlotNPIFits.r
@@ -0,0 +1,21 @@
+#dat = read.csv('latpositions98-14.csv')
+
+# Latitude of 18C at 157.5-158.5W average
+par(mfrow=c(1,3))
+plot(dat$NPI,dat$S18,pch=21,bg='grey',xlab='NPI',ylab='Latitude (N)',main='18C Latitude (157.5-158.5W AVG)')
+S.fit = lm(S18~NPI,data=dat)
+abline(S.fit)
+text(1011.5, 29, sprintf('r2=%5.2f, p=%s', summary(S.fit)$adj.r.squared, sprintf('%6.4f',summary(S.fit)$coeff[8])))
+
+# Latitude of 0.2 mg m-3 at 157.5-158.5W average
+plot(dat$NPI,dat$CP2,pch=21,bg='grey',xlab='NPI',ylab='Latitude (N)',main='0.2 mg m-3 Latitude (157.5-158.5W AVG)')
+C.fit = lm(CP2~NPI,data=dat)
+abline(C.fit)
+text(1011.5, 30.5, sprintf('r2=%5.2f, p=%s', summary(C.fit)$adj.r.squared, sprintf('%6.4f',summary(C.fit)$coeff[8])))
+
+# Latitude of 0.2 mg m-3 minus latitude of 18C at 157.5-158.5W average
+plot(dat$NPI,dat$FrontDiff,pch=21,bg='grey',xlab='NPI',ylab=' D Latitude',main='0.2 Lat - 18 Lat (157.5-158.5W AVG)')
+D.fit = lm(FrontDiff~NPI,data=dat)
+abline(D.fit)
+text(1011.5, 0.75, sprintf('r2=%5.2f, p=%s', summary(D.fit)$adj.r.squared, sprintf('%6.4f',summary(D.fit)$coeff[8])))
+
